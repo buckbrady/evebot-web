fba6a73567dc40c5bd8aad71291b8641f213aa8f
Buck Brady <bbrady145@gmail.com>
2023-08-06 19:41:45 -0600

    feat: add sync status page

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,5 +18,6 @@ module.exports = {
     quotes: ['error', 'single'],
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 }
--- src/pages/status/index.tsx
+++ src/pages/status/index.tsx
@@ -1,8 +1,19 @@
 import { gql, useQuery } from '@apollo/client'
+import {
+  Container,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Th,
+  Thead,
+  Tr
+} from '@chakra-ui/react'
+import { DateTime } from 'luxon'
 
 const GET_SYNC_STATUS = gql`
   query GetSyncStatus {
-    syncStatus {
+    syncStatus(orderBy: { interval: ASC }) {
       name
       lastSync
       nextSync
@@ -11,16 +22,49 @@ const GET_SYNC_STATUS = gql`
   }
 `
 const StatusPage = () => {
-  const { data, loading, error } = useQuery(GET_SYNC_STATUS)
+  const { data, loading, error } = useQuery(GET_SYNC_STATUS, {
+    pollInterval: 5000
+  })
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
 
   return (
-    <div>
-      <h1>Status Page</h1>
-      {data.syncStatus}
-    </div>
+    <Container maxW={'70%'}>
+      <TableContainer>
+        <Table variant={'simple'}>
+          <TableCaption placement={'top'}>Sync Status</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Resource</Th>
+              <Th>Until Next Sync</Th>
+              <Th>Last Sync</Th>
+              <Th>Next Sync</Th>
+              <Th>Interval</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data.syncStatus.map((status: any) => (
+              <Tr key={status.name}>
+                <Th>{status.name}</Th>
+                <Th>{DateTime.fromISO(status.nextSync).toRelative()}</Th>
+                <Th>
+                  {DateTime.fromISO(status.lastSync).toLocaleString(
+                    DateTime.DATETIME_FULL_WITH_SECONDS
+                  )}
+                </Th>
+                <Th>
+                  {DateTime.fromISO(status.nextSync).toLocaleString(
+                    DateTime.DATETIME_FULL_WITH_SECONDS
+                  )}
+                </Th>
+                <Th>{status.interval}</Th>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Container>
   )
 }
 
